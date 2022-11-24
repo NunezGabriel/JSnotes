69fbf410594152d59f325c8c2ee99bf8fee15251
Gabriel Nunez <emilio.nunez.arenas@gmail.com>
2022-11-24 12:08:46 -0500

    hola de nuevo

---
+++ 1-Datos.js
@@ -0,0 +1,17 @@
+/*tipos de datos y valores en js*/
+
+/*daros primitivos*/
+a=5 /*number */
+b="hola" /*string */
+c=true/*boolean */
+d=null /*object -> sale object prq es una de las muchass cosas raras de JS xd*/
+e=undefined /*undefined */
+
+/*datos no primitivos*/
+console.log("hola")/*function */
+{tipo:"objeto"}/*object */
+f=[1,2,3,"hola"]/*object */
+
+
+
+
---
+++ 2-variables.js
@@ -0,0 +1,31 @@
+//LAS 2 FASES DE USO DE UNA VARIABLE
+
+//declarar la variable
+var altura;
+
+//inicializar la variable
+altura = 200
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+var nombre = "Oscar"
+var nombre2 = "juan"
+console.log(nombre, "y", nombre2, "van a jugar Dota2" ) //Oscar y juan van a jugar Dota2
+
+const x = 12
+const y = 15
+console.log(x + y + (x*y))//207
+
+let apodo = "cabezon"
+let edad = 20
+console.log(nombre, "el", apodo, "tiene", edad, "años") //Oscar el cabezon tiene 20 años
+
+let frutas = ['mazana', 'pera', 'platanao', 'arandano'] //una variable de tipo array
+console.log(frutas[1])
+
+let persona = { //variable de tipo objeto
+    nombre: 'diego',
+    edad: 30
+}
+console.log(persona.nombre)
+
