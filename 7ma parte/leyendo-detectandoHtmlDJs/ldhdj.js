3847e84e389f4dbb0f78af197413b085e54d04e1
Gabriel Nunez <emilio.nunez.arenas@gmail.com>
2023-01-27 15:08:58 -0500

    agregando datos de archivo

--- 7ma parte/leyendo-detectandoHtmlDJs/ldhdj.js
+++ 7ma parte/leyendo-detectandoHtmlDJs/ldhdj.js
@@ -1,1 +1,16 @@
-document.write('hola')
+let h1Container = document.querySelector('h1') //con esto asignamos a la etiqueta html una variable del nombre que queramos
+let pContainer = document.querySelector('p')
+let spanContainer = document.querySelector('span')
+let inputContainer = document.querySelector('input')
+
+console.log({// y al imprimir como objeto las variables podemos ver lo que almacenan h1Container almacena la etiqueta h1
+    h1Container,
+    pContainer,
+    spanContainer,
+})
+console.log(inputContainer.value)// esto imprime el valor que tiene el input que en este caso le pusimos valor secreto
+/*
+OUTPUT EN CONSOLA
+-> {h1Container: h1, pContainer: p, spanContainer: span}[Prototype]]: Object
+-> valorsecreto
+*/
