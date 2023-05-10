3c3683f165483cfaa5d273cb074b761396de86b9
Gabriel <emilio.nunez.arenas@gmail.com>
2023-05-09 22:52:43 -0500

    chore:stringMethods

--- 7ma parte/1-stringMethods.js
+++ 7ma parte/1-stringMethods.js
@@ -18,4 +18,14 @@ mensaje.endsWith(mensaje2) // lo mismo que stratsWith pero el final
 mensaje.includes(mensaje2) // si una cadena se encuentra de otra cadena devuelve true
 mensaje.indexOf('de') //devuelve el index de lo que pongamos en su inicio en este caso retorna 9
 mensaje.lastIndexOf('de') //devuelve el index de lo que pongamos en su final en este caso retorna 10
-
+mensaje.padStart(36,'e') //rellena la cadena al principio con los caracteres deseados
+mensaje.padEnd(36,'e') //lo mismo que padstart pero al final
+mensaje.repeat(2) //repite el string la cantidad de veces que se le indique
+mensaje.split(',') //divide la cadena como le pidamos en este caso va a dividir cada caracter por comas
+mensaje.substring(1,3) //desde que posicion hasta qye posicion  quieres que se vea tu string
+mensaje.toLowerCase() // convierte el string a minuscula
+mensaje.toUpperCase() //convierte el string a mayuscula
+mensaje.toString() // convierte a string xd
+mensaje.trim()//elimina los espacios en blanco
+mensaje.trimEnd()//elimina los espacios del final
+mensaje.trimStart()//lo mismo pero del comienzo
